9538a2362bc21d1e5ee010f5cf3e72eaf7711661
Vitaliy <parshyn013@gmail.com>
2020-01-30 20:05:23 +0300

    init

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+	// for smooth scroll
+    smoothScroll.init({
+        selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
+        speed: 500, // Integer. How fast to complete the scroll in milliseconds
+        easing: 'easeInQuad', // Easing pattern to use
+        offset: 50 // Integer. How far to offset the scrolling anchor location in pixels
+    });
+});
